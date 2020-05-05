d6b8cf327d65ce6d0f8b8616970065c4966c7b13
HoarFox <kavokinm@gmail.com>
2020-05-05 22:50:21 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 let active = { empty: true };
 let modal = false;
 
-const url = "https://word-cards-15-12-2019.herokuapp.com"; // "http://localhost:5000"
+const url = "http://localhost:5000"; // "https://word-cards-15-12-2019.herokuapp.com"
 const hashValues = {
   start: "#start",
   home: "#home",
